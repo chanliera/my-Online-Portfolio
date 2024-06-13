1a800213de8750df73fd931853795a94caa4f39d
Christian dev <132464401+chanliera@users.noreply.github.com>
2024-06-13 18:45:04 +0800

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,6 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const preloader = document.querySelector(".preloader");
+  preloader.style.opacity = 0;
+  preloader.style.transition = "opacity 4s ease-in-out"; // Fade out in 4 seconds
+});
